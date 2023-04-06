e64ebbeaa710cd658756dba1f9ec321f05fd7e19
InnaShtondenko <inna.mycase@gmail.com>
2023-04-06 19:06:57 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,33 @@
-const express = require('express')
+const express = require("express");
+const {
+  getContactsController,
+  getContactByIdController,
+  addContactController,
+  removeContactController,
+  updateContactController,
+  updateStatusContact,
+} = require("../../controllers/contactsController");
+const { asyncWrapper } = require("../../helpers/apiHelpers");
+const {
+  newContactValidaion,
+  updatedContactValidation,
+  updateStatusContactValidation,
+} = require("../../middlewares/validationMiddleware");
+const router = express.Router();
 
-const router = express.Router()
+router.get("/", asyncWrapper(getContactsController));
+router.get("/:contactId", asyncWrapper(getContactByIdController));
+router.post("/", newContactValidaion, asyncWrapper(addContactController));
+router.delete("/:contactId", asyncWrapper(removeContactController));
+router.put(
+  "/:contactId",
+  updatedContactValidation,
+  asyncWrapper(updateContactController)
+);
+router.patch(
+  "/:contactId/favorite",
+  updateStatusContactValidation,
+  asyncWrapper(updateStatusContact)
+);
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
